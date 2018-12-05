dec3248745ae445bca4ceeec80562533096f6526
Bartek Paczkowski <bartek.paczkowski20@gmail.com>
2018-12-05 15:57:37 +0100

    made sorting alphabetic

--- src/org/sylfra/idea/plugins/linessorter/SortUtils.java
+++ src/org/sylfra/idea/plugins/linessorter/SortUtils.java
@@ -1,8 +1,7 @@
 package org.sylfra.idea.plugins.linessorter;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.text.Collator;
+import java.util.*;
 
 /**
  * Convenient class for sort functions
@@ -12,17 +11,16 @@ import java.util.List;
 public class SortUtils
 {
   private static final Comparator<String> DEFAULT_COMPARATOR = new DefaultComparator();
+  private static final Collator ENGLISH_COLLATOR = Collator.getInstance(new Locale("en", "US"));
 
   public static void defaultSort(List<String> lines)
   {
-    Collections.sort(lines, DEFAULT_COMPARATOR);
+    lines.sort(DEFAULT_COMPARATOR);
   }
 
-  private static class DefaultComparator implements Comparator<String>
-  {
-    public int compare(String s1, String s2)
-    {
-      return s1.compareToIgnoreCase(s2);
+  private static class DefaultComparator implements Comparator<String> {
+    public int compare(String s1, String s2) {
+      return ENGLISH_COLLATOR.compare(s1, s2);
     }
   }
 }
